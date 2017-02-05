c3bb0c5ecd53d7dc3c64a9929d7685c80da2c42a
Amit <shinde.amit27031991@gmail.com>
2017-02-05 23:01:17 +0900

    improve naming in fibonacci sequence

--- searching_algorithms/fibonacci_sequence.py
+++ searching_algorithms/fibonacci_sequence.py
@@ -2,9 +2,9 @@
 # Generate Fibonacci sequence of length N
 # ------------------------
 
-def fibo(n):
-  fibo_arr = [0, 1]
+def fibonacci_sequence(n):
+  sequence = [0, 1]
 
   for i in range(2, n):
-    fibo_arr.append(fibo_arr[-1] + fibo_arr[-2])
-  return fibo_arr[:n]
+    sequence.append(sequence[-1] + sequence[-2])
+  return sequence[:n]
